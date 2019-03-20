739a3d919c1aa15d6bcf1989c39df2f45c79e462
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2019-03-20 08:57:33 -0500

    Add homebrew formula

---
+++ hellobrew.rb
@@ -0,0 +1,13 @@
+class Hellobrew < Formula
+  desc "Trying out homebrew taps"
+  homepage "https://github.com/e1senh0rn/hellobrew"
+  url "https://github.com/e1senh0rn/hellobrew/releases/download/v0.1.0/hellobrew-Darwin-x86_64"
+  sha256 "d5298aa79e04e52c2144fbf4056b38f877f657e0808d199c3fb524c268eec503"
+  head "https://github.com/e1senh0rn/hellobrew.git"
+
+  bottle :unneeded
+
+  def install
+    bin.install "hellobrew"
+  end
+end
